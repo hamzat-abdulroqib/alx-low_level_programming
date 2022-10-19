aeac2bdf57ec7cc3831dcefdfa6bc3dae813ebb8
hamzat <binhamzat0@gmail.com>
2022-10-19 08:31:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabets in lowercase
+ *
+ * return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
